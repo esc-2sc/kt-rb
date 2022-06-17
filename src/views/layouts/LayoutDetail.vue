6c742129cc50e8d14cfc9d0d11b10ae4ab5c37e6
mihee <an1025@finenuts.co.kr>
2022-06-17 15:47:20 +0900

    VueRangedatePicker 달력 추가

--- src/views/layouts/LayoutDetail.vue
+++ src/views/layouts/LayoutDetail.vue
@@ -3,6 +3,8 @@
         <input type="text" id="litepicker">
         <header>
             <navigation></navigation>
+          <br>
+          <vue-rangedate-picker></vue-rangedate-picker>
         </header>
         <div class="contents">
             <router-view></router-view>
@@ -13,11 +15,13 @@
 <script>
 import Litepicker from 'litepicker';
 import Navigation from "@/views/layouts/Navigation";
+import VueRangedatePicker from 'vue-rangedate-picker';
 
 export default {
 	name: 'layout-detail',
 	components: {
         Navigation,
+        VueRangedatePicker
 	},
     data() {
         return {

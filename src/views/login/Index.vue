c812a5ab170ee074af7c03bcf7390c1dd08027f9
esc <id800910@gmail.com>
2022-06-17 16:37:48 +0900

    - 캘린더 위치 변경

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>login content</div>
+    <div>
+        login content
+        <input type="text" id="litepicker">
+        <vue-rangedate-picker></vue-rangedate-picker>
+    </div>
 </template>
 
 <script>
+import Litepicker from 'litepicker';
+import VueRangedatePicker from 'vue-rangedate-picker';
 export default {
     name: 'login',
+    components: {VueRangedatePicker},
 	data() {
 		return {};
 	},
 	methods: {
 	},
+    mounted() {
+    const picker = new Litepicker({
+      element: document.getElementById('litepicker')
+    });
+    }
 };
 </script>
 

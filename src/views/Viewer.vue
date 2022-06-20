6ac34d49d92f375c6bf2e05688c6638a9f13ba8b
esc <id800910@gmail.com>
2022-06-20 17:12:51 +0900

    - chart nav 작업중

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -166,7 +166,179 @@
                 </div>
             </div>
             <div class="timeline-chart">
-                <div class="timeline-nav"><img src="/resources/images/temp/temp_chart_timeline.png" alt=""></div>
+                <div class="timeline-nav">
+                    <ul class="chart-nav">
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:15%;"></span>
+                                <span class="bar-front" style="height:70%;"></span>
+                            </div>
+                            <div class="bar-label">1시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">2시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">3시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">4시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">5시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">6시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">7시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">8시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">9시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">10시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">11시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">12시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">13시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">14시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">15시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">16시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">17시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">18시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">19시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">20시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">21시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:100%;"></span>
+                                <span class="bar-front" style="height:30%;"></span>
+                            </div>
+                            <div class="bar-label">22시</div>
+                        </a></li>
+                        <li><a href="#" class="selected">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">23시</div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="bar-area">
+                                <span class="bar-back" style="height:30%;"></span>
+                                <span class="bar-front" style="height:100%;"></span>
+                            </div>
+                            <div class="bar-label">24시</div>
+                        </a></li>
+                    </ul><!-- //chart-nav -->
+                    <!-- img src="/resources/images/temp/temp_chart_timeline.png" alt="" -->
+                </div>
             </div>
         </div><!-- //control-timeline -->
         <div class="map-info-window" :class="{'open': isActive}">

5e4dc93fd37434459124f54ce35049e01997a775
mihee <an1025@finenuts.co.kr>
2022-06-23 16:48:37 +0900

    - r-popup 추가

--- src/views/guide/Rpopup.vue
+++ src/views/guide/Rpopup.vue
@@ -3,7 +3,21 @@
         <guide-box-item title="Default">
 
             <div class="r-popup">
-                r-popup
+              <div class="r-popup__header">
+                <div class="title">Title</div>
+                <div class="func">
+                  <button class="btn-close"><img src="/resources/images/svg/icon-close.svg" alt="close"></button>
+                </div>
+              </div><!-- //r-popup__header -->
+              <div class="r-popup__body">
+                <div style="text-align:center">body</div>
+              </div><!-- // r-popup__body -->
+              <div class="r-popup__footer">
+                <div class="btn-wrap right">
+                  <button type="button" class="a-btn-type-1 color--white">취소</button>
+                  <button type="button" class="a-btn-type-1 color--blue">권한변경</button>
+                </div>
+              </div><!-- r-popup__footer -->
             </div><!-- //r-popup -->
 
         </guide-box-item>
